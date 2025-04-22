216576f422e987cf571bbb04550aed9b01d084ab
Bugstacker <kennethmtv.km@gmail.com>
2025-04-22 19:53:19 +0300

    Adding the the website URL to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next"
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: 'https://kennethmatovu.vercel.app',
+            url: "https://www.emkaydev.me",
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 1.0,
